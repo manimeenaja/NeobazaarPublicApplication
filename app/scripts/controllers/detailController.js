0f9920cae28f37b2641a89589688ce2a1ae96b5c
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-08 23:19:00 +0100

    fb og

--- app/scripts/controllers/detailController.js
+++ app/scripts/controllers/detailController.js
@@ -1,13 +1,15 @@
 'use strict';
 
-angular.module('Neobazaar').controller('DetailController',
-    function($scope, $http, $location, $window, ClassifiedLoader) {
+angular.module('Neobazaar').controller(
+    'DetailController',
+    function($scope, $rootScope, $http, $location, $window, ClassifiedLoader) {
       $scope.resource = new ClassifiedLoader();
       $scope.path = $location.path();
-      
+
       $scope.resource.then(function(response) {
         $window.document.title = response.data.title + " - "
             + $window.document.title;
+        $rootScope.title = $window.document.title;
       });
 
       $scope.back = function() {
