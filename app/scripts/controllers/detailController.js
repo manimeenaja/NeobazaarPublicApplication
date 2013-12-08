f4a0c07be6df1a89e65fceb97fd45335d0af324d
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-08 20:46:48 +0100

    detail fix

--- app/scripts/controllers/detailController.js
+++ app/scripts/controllers/detailController.js
@@ -1,12 +1,18 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('DetailController', function ($scope, $http, $location, $window, ClassifiedLoader) {
-	$scope.resource = new ClassifiedLoader();
-	$scope.path = $location.path();
-	
-	$scope.back = function() {
-		$window.history.back();
-	}
+angular.module('Neobazaar').controller('DetailController',
+    function($scope, $http, $location, $window, ClassifiedLoader) {
+      $scope.resource = new ClassifiedLoader();
+      $scope.path = $location.path();
+      
 
-  });
+      $scope.resource.then(function(response) { 
+        console.log(response);
+        console.log($scope.resource);
+      });
+
+      $scope.back = function() {
+        $window.history.back();
+      }
+
+    });
