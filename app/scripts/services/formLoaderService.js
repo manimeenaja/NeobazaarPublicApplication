3fb79bbe68e25f57a6f5e8c0db1b8637ed072c17
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 00:00:50 +0100

    jshint services/formLoaderService.js

--- app/scripts/services/formLoaderService.js
+++ app/scripts/services/formLoaderService.js
@@ -2,15 +2,14 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('FormLoader', ['Form', '$q',
-    function (Form, $q) {
-        return function (params) {
-            var delay = $q.defer();
-            Form.get(params, function (form) {
-                delay.resolve(form);
-            }, function () {
-                delay.reject('Unable to fetch form ' + params.category);
-            });
-            return delay.promise;
-        };
-    }]);
+services.factory('FormLoader', [ 'Form', '$q', function(Form, $q) {
+  return function(params) {
+    var delay = $q.defer();
+    Form.get(params, function(form) {
+      delay.resolve(form);
+    }, function() {
+      delay.reject('Unable to fetch form ' + params.category);
+    });
+    return delay.promise;
+  };
+} ]);
