b63d519f050f0c6698e78225c14e48a22d485371
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 00:56:20 +0100

    jshint services/classifiedsLoaderService.js

--- app/scripts/services/classifiedLoaderService.js
+++ app/scripts/services/classifiedLoaderService.js
@@ -2,15 +2,23 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('ClassifiedLoader', ['Classified', '$route', '$q',
-    function (Classified, $route, $q) {
-        return function () {
-            var delay = $q.defer();
-            Classified.get({id: $route.current.params.id}, function (classified) {
-                delay.resolve(classified);
-            }, function () {
-                delay.reject('Unable to fetch classified ' + $route.current.params.id);
+services.factory('ClassifiedLoader', [
+    'Classified',
+    '$route',
+    '$q',
+    function(Classified, $route, $q) {
+      return function() {
+        var delay = $q.defer();
+        Classified.get({
+          id : $route.current.params.id
+        }, function(classified) {
+          delay.resolve(classified);
+        },
+            function() {
+              delay.reject('Unable to fetch classified ' +
+                  $route.current.params.id);
             });
-            return delay.promise;
-        };
-    }]);
+        return delay.promise;
+      };
+    }
+  ]);
