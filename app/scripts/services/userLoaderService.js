674d0cd3be159846c8d418e7ce4d4cf8e59d6287
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-21 23:43:48 +0100

    jshint services/userLoaderService.js

--- app/scripts/services/userLoaderService.js
+++ app/scripts/services/userLoaderService.js
@@ -2,15 +2,16 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('UserLoader', ['User', '$q',
-    function (User, $q) {
-        return function (params) {
-            var delay = $q.defer();
-            User.get({id: params.id}, function (user) {
-                delay.resolve(user);
-            }, function () {
-                delay.reject('Unable to fetch user ' + params.id);
-            });
-            return delay.promise;
-        };
-    }]);
+services.factory('UserLoader', [ 'User', '$q', function(User, $q) {
+  return function(params) {
+    var delay = $q.defer();
+    User.get({
+      id : params.id
+    }, function(user) {
+      delay.resolve(user);
+    }, function() {
+      delay.reject('Unable to fetch user ' + params.id);
+    });
+    return delay.promise;
+  };
+} ]);
