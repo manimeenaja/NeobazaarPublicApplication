d8a5063ab25945403e30b930037fcd6d26ad0513
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-21 23:38:34 +0100

    jshint services/userService.js

--- app/scripts/services/userService.js
+++ app/scripts/services/userService.js
@@ -2,15 +2,37 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('User', ['$resource', '$route', 
-    function ($resource, $route) {	
-        return $resource('/user/rest/:id', 
-        		{ id: '@id'},
-        		{
-        			activate: {method:'PUT', params:{'action':'activation'}, isArray:false}, 
-        			passwordRecovered: {method:'PUT', params:{'action':'passwordRecovered'}, isArray:false},
-        			disableEnable: {method:'PUT', params:{'action': 'disableEnable'}, isArray:false},
-        			ban: {method:'PUT', params:{'action': 'banunban'}, isArray:false}
-        		}
-       );
-    }]);
+services.factory('User', [ '$resource', function($resource) {
+  return $resource('/user/rest/:id', {
+    id : '@id'
+  }, {
+    activate : {
+      method : 'PUT',
+      params : {
+        'action' : 'activation'
+      },
+      isArray : false
+    },
+    passwordRecovered : {
+      method : 'PUT',
+      params : {
+        'action' : 'passwordRecovered'
+      },
+      isArray : false
+    },
+    disableEnable : {
+      method : 'PUT',
+      params : {
+        'action' : 'disableEnable'
+      },
+      isArray : false
+    },
+    ban : {
+      method : 'PUT',
+      params : {
+        'action' : 'banunban'
+      },
+      isArray : false
+    }
+  });
+} ]);
