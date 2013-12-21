16cbcd5d1f01e482ba70848002c2a69937b1b7b8
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-21 23:35:57 +0100

    grunt and tests config

--- app/scripts/services/usersLoaderService.js
+++ app/scripts/services/usersLoaderService.js
@@ -2,15 +2,14 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('UsersLoader', ['User', '$q',            
-    function (User, $q) {
-        return function (params) {
-            var delay = $q.defer();
-            User.get(params, function (user) {
-                delay.resolve(user);
-            }, function () {
-                delay.reject('Unable to fetch user');
-            });
-            return delay.promise;
-        };
-    }]);
+services.factory('UsersLoader', [ 'User', '$q', function(User, $q) {
+  return function(params) {
+    var delay = $q.defer();
+    User.get(params, function(user) {
+      delay.resolve(user);
+    }, function() {
+      delay.reject('Unable to fetch user');
+    });
+    return delay.promise;
+  };
+} ]);
--- karma.conf.js
+++ karma.conf.js
@@ -13,11 +13,18 @@ module.exports = function(config) {
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.min.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js',
+      
+      // Inserire qui tutto quello che serve all'app
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-cookies/angular-cookies.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
+      'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/angular-ui/build/angular-ui.js',
+      'app/bower_components/angular-local-storage/angular-local-storage.js'
     ],
 
     // list of files / patterns to exclude
