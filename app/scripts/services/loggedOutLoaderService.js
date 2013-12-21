df3c0ad0eff0e5a635f2f5e4e6e6050dd4c155b0
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-21 23:49:43 +0100

    jshint services/loggedOutLoaderService.js

--- app/scripts/services/loggedOutLoaderService.js
+++ app/scripts/services/loggedOutLoaderService.js
@@ -2,15 +2,15 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('LoggedOutLoader', ['LoggedOut', '$q',            
-    function (LoggedOut, $q) {
-        return function () {
-            var delay = $q.defer();
-            LoggedOut.get({}, function (loggedOut) {
-                delay.resolve(loggedOut);
-            }, function () {
-                delay.reject('Unable to fetch LoggedOut');
-            });
-            return delay.promise;
-        };
-    }]);
+services.factory('LoggedOutLoader', [ 'LoggedOut', '$q',
+    function(LoggedOut, $q) {
+      return function() {
+        var delay = $q.defer();
+        LoggedOut.get({}, function(loggedOut) {
+          delay.resolve(loggedOut);
+        }, function() {
+          delay.reject('Unable to fetch LoggedOut');
+        });
+        return delay.promise;
+      };
+    } ]);
