09c70c8ca4e3ce810f695578c092b16be6c70d6b
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 00:02:15 +0100

    jshint services/csfrLoaderService.js

--- app/scripts/services/csrfLoaderService.js
+++ app/scripts/services/csrfLoaderService.js
@@ -2,15 +2,14 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('CsrfLoader', ['Csrf', '$q',
-    function (Csrf, $q) {
-        return function () {
-            var delay = $q.defer();
-            Csrf.get({}, function (csrf) {
-                delay.resolve(csrf);
-            }, function () {
-                delay.reject('Unable to fetch Csrf');
-            });
-            return delay.promise;
-        };
-    }]);
+services.factory('CsrfLoader', [ 'Csrf', '$q', function(Csrf, $q) {
+  return function() {
+    var delay = $q.defer();
+    Csrf.get({}, function(csrf) {
+      delay.resolve(csrf);
+    }, function() {
+      delay.reject('Unable to fetch Csrf');
+    });
+    return delay.promise;
+  };
+} ]);
