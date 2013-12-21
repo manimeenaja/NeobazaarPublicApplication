88fe85c70e5a0d203aeda90bcc4cdfb6e5887e2e
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 00:51:09 +0100

    jshint services/classifiedsLoaderService.js

--- app/scripts/services/classifiedsLoaderService.js
+++ app/scripts/services/classifiedsLoaderService.js
@@ -2,15 +2,15 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('ClassifiedsLoader', ['Classified', '$q',            
-    function (Classified, $q) {
-        return function (params) {
-            var delay = $q.defer();
-            Classified.get(params, function (classified) {
-                delay.resolve(classified);
-            }, function () {
-                delay.reject('Unable to fetch classified');
-            });
-            return delay.promise;
-        };
-    }]);
+services.factory('ClassifiedsLoader', [ 'Classified', '$q',
+    function(Classified, $q) {
+      return function(params) {
+        var delay = $q.defer();
+        Classified.get(params, function(classified) {
+          delay.resolve(classified);
+        }, function() {
+          delay.reject('Unable to fetch classified');
+        });
+        return delay.promise;
+      };
+    } ]);
