aee67a01fde1a494a74a6cd0c52661ca204061ed
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 00:58:38 +0100

    jshint services/categoriesLoaderService.js

--- app/scripts/services/categoriesLoaderService.js
+++ app/scripts/services/categoriesLoaderService.js
@@ -2,15 +2,15 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('CategoriesLoader', ['Categories', '$q',
-    function (Categories, $q) {
-        return function () {
-            var delay = $q.defer();
-            Categories.get({}, function (categories) {
-                delay.resolve(categories);
-            }, function () {
-                delay.reject('Unable to fetch Categories');
-            });
-            return delay.promise;
-        };
-    }]);
+services.factory('CategoriesLoader', [ 'Categories', '$q',
+    function(Categories, $q) {
+      return function() {
+        var delay = $q.defer();
+        Categories.get({}, function(categories) {
+          delay.resolve(categories);
+        }, function() {
+          delay.reject('Unable to fetch Categories');
+        });
+        return delay.promise;
+      };
+    } ]);
