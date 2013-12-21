8e5072ce64da4409606d88c65c1411df031fbf64
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-21 23:53:09 +0100

    jshint services/loggedInLoaderService.js

--- app/scripts/services/loggedInLoaderService.js
+++ app/scripts/services/loggedInLoaderService.js
@@ -2,15 +2,14 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('LoggedInLoader', ['LoggedIn', '$q',            
-    function (LoggedIn, $q) {
-        return function () {
-            var delay = $q.defer();
-            LoggedIn.get({}, function (loggedIn) {
-                delay.resolve(loggedIn);
-            }, function () {
-                delay.reject('Unable to fetch LoggedIn');
-            });
-            return delay.promise;
-        };
-    }]);
+services.factory('LoggedInLoader', [ 'LoggedIn', '$q', function(LoggedIn, $q) {
+  return function() {
+    var delay = $q.defer();
+    LoggedIn.get({}, function(loggedIn) {
+      delay.resolve(loggedIn);
+    }, function() {
+      delay.reject('Unable to fetch LoggedIn');
+    });
+    return delay.promise;
+  };
+} ]);
