931c47a80224ca5a85bd23cac667b736923e9733
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 03:05:22 +0100

    jshint directives/lightboxDirective.js

--- app/scripts/directives/lightboxDirective.js
+++ app/scripts/directives/lightboxDirective.js
@@ -2,12 +2,12 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('lightbox', function () {
-	  var directiveDefinitionObject =  {
-			restrict: 'A',
-			link: function(scope, element, attrs) {
-			    $(element).lightbox(scope.$eval(attrs.directiveName));
-			}
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('lightbox', function() {
+  var directiveDefinitionObject = {
+    restrict : 'A',
+    link : function(scope, element, attrs) {
+      angular.element(element).lightbox(scope.$eval(attrs.directiveName));
+    }
+  };
+  return directiveDefinitionObject;
+});
