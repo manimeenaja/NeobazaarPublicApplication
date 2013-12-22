4a177df3999f501f064d1105c2bdf33ef9595b50
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:21:50 +0100

    jshint controllers/userActivationController.js

--- app/scripts/controllers/userActivationController.js
+++ app/scripts/controllers/userActivationController.js
@@ -1,23 +1,27 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('UserActivationController', function ($scope, $routeParams, $route, $rootScope, $location, $timeout, User) {
-	  if($rootScope.logged) {
-		  $location.path('/login');
-		  return;
-	  }
-		
-	  $scope.data = false;
-	  $scope.activate = function(id) {
-		  User.activate({'id':id}, function(data) {
-			  $scope.data = data;
-		  }, function(data) {
-			  $scope.data = data.data;
-		  });
-	  }
-	  $scope.activate($routeParams.id);
+angular.module('Neobazaar').controller(
+    'UserActivationController',
+    function($scope, $routeParams, $route, $rootScope, $location, $timeout,
+        User) {
+      if ($rootScope.logged) {
+        $location.path('/login');
+        return;
+      }
 
-	  $scope.getTemplateUrl = function() {
-		  return '/app/views/loggedout/user-activation.html';
-	  }
-  });
+      $scope.data = false;
+      $scope.activate = function(id) {
+        User.activate({
+          'id' : id
+        }, function(data) {
+          $scope.data = data;
+        }, function(data) {
+          $scope.data = data.data;
+        });
+      };
+      $scope.activate($routeParams.id);
+
+      $scope.getTemplateUrl = function() {
+        return '/app/views/loggedout/user-activation.html';
+      };
+    });
