b9bf03385f155eefc6ecb38347c9b53eabf815c8
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 02:59:14 +0100

    jshint directives/searchFormDirective.js

--- app/scripts/directives/searchFormDirective.js
+++ app/scripts/directives/searchFormDirective.js
@@ -6,16 +6,14 @@ var directives = angular.module('Neobazaar.directives', []);
 //	  $templateCache.put('helloTemplateCached.html', '/app/views/searchform.html');
 //});
 
-directives.directive('neoSearchForm', function () {
-	  var directiveDefinitionObject =  {
-          restrict: 'E',
-          priority: 1000,
-          scope: true,
-          templateUrl: '/app/views/searchform.html',
-          //template: '<div ng-include="getFormTemplateUrl()"></div>',
-          controller: 'SearchFormController',
-          link: function(scope, iElement, iAttrs) {
-          }
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('neoSearchForm', function() {
+  var directiveDefinitionObject = {
+    restrict : 'E',
+    priority : 1000,
+    scope : true,
+    templateUrl : '/app/views/searchform.html',
+    //template: '<div ng-include="getFormTemplateUrl()"></div>',
+    controller : 'SearchFormController'
+  };
+  return directiveDefinitionObject;
+});
