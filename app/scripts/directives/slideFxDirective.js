2af6ae3b331cd755d70761bb39bb23151a52b89e
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 02:54:25 +0100

    jshint directives/slideFxDirective.js

--- app/scripts/directives/slideFxDirective.js
+++ app/scripts/directives/slideFxDirective.js
@@ -2,17 +2,17 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('slidefx', function () {
-	  var directiveDefinitionObject =  {
-			restrict: 'A',
-			link: function(scope, element, attrs) {
-				$(element).click(function(e) {
-					$(attrs.href).slideToggle("slow", function() {
-						// Animation complete.
-					});
-					e.preventDefault();
-				});
-			}
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('slidefx', function() {
+  var directiveDefinitionObject = {
+    restrict : 'A',
+    link : function(scope, element, attrs) {
+      angular.element(element).click(function(e) {
+        angular.element(attrs.href).slideToggle('slow', function() {
+          // Animation complete.
+        });
+        e.preventDefault();
+      });
+    }
+  };
+  return directiveDefinitionObject;
+});
