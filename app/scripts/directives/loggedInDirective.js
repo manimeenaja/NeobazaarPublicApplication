08a6eb8bef5bd35f298ae04e0e40bac5b9129af9
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 03:03:27 +0100

    jshint directives/loggedInDirective.js

--- app/scripts/directives/loggedInDirective.js
+++ app/scripts/directives/loggedInDirective.js
@@ -2,13 +2,13 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('loggedIn', function () {
-	  var directiveDefinitionObject =  {
-          restrict: 'E',
-          priority: 1000,
-          scope: true,
-          template: '<div ng-include="getTemplateUrl()"></div>',
-          controller: 'LoggedInController'
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('loggedIn', function() {
+  var directiveDefinitionObject = {
+    restrict : 'E',
+    priority : 1000,
+    scope : true,
+    template : '<div ng-include="getTemplateUrl()"></div>',
+    controller : 'LoggedInController'
+  };
+  return directiveDefinitionObject;
+});
