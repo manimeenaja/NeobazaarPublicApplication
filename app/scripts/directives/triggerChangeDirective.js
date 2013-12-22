a1d1a24d9e63700cc46c68a8e0f6c2555410abf2
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 02:40:28 +0100

    jshint directives/triggerChangeDirective.js

--- app/scripts/directives/triggerChangeDirective.js
+++ app/scripts/directives/triggerChangeDirective.js
@@ -2,14 +2,15 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('triggerChange', function ($sniffer) {
-	  var directiveDefinitionObject =  {
-			link: function(scope, element, attrs) {
-				element.bind('click', function(){
-		            $(attrs.triggerChange).trigger($sniffer.hasEvent('input') ? 'input' : 'change');
-		        });
-			},
-		    priority : 1
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('triggerChange', function($sniffer) {
+  var directiveDefinitionObject = {
+    link : function(scope, element, attrs) {
+      element.bind('click', function() {
+        angular.element(attrs.triggerChange).trigger(
+            $sniffer.hasEvent('input') ? 'input' : 'change');
+      });
+    },
+    priority : 1
+  };
+  return directiveDefinitionObject;
+});
