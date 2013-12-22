822f406b48df50a5f50aeb5f69793ceb01f6d0dd
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 19:38:13 +0100

    jshint controllers/headerController.js

--- app/scripts/controllers/headerController.js
+++ app/scripts/controllers/headerController.js
@@ -1,18 +1,18 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('HeaderController', function ($scope, $routeParams) {
-	  $scope.params = $routeParams;
-	  
-	  $scope.heading = 'Descrizione questa pagina';
-	  
-	  $scope.getHeaderTemplateUrl =  function() {
-		  return '/app/views/header-search.html';
-//		  return 'location' in $routeParams || 'id' in $routeParams ? 
-//				  '/app/views/header-search.html' : '/app/views/header-home.html';
-	  }
-	  
-	  $scope.$watch('params.location.value', function() {
-		  $scope.getHeaderTemplateUrl();
-	  });
-  });
+angular.module('Neobazaar').controller('HeaderController',
+    function($scope, $routeParams) {
+      $scope.params = $routeParams;
+
+      $scope.heading = 'Descrizione questa pagina';
+
+      $scope.getHeaderTemplateUrl = function() {
+        return '/app/views/header-search.html';
+        // return 'location' in $routeParams || 'id' in $routeParams ?
+        // '/app/views/header-search.html' : '/app/views/header-home.html';
+      };
+
+      $scope.$watch('params.location.value', function() {
+        $scope.getHeaderTemplateUrl();
+      });
+    });
