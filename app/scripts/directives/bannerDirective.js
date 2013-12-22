93d628e9f2d571cfb823eb4596cdb1bf8491b446
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:18:20 +0100

    jshint directives/bannerDirective.js

--- app/scripts/directives/bannerDirective.js
+++ app/scripts/directives/bannerDirective.js
@@ -2,16 +2,14 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('banner', function () {
-	  var directiveDefinitionObject =  {
-          restrict: 'E',
-          priority: 1000,
-          scope: true,
-          //templateUrl: '/app/views/contact-form.html',
-          template: '<div ng-include="getTemplateUrl()"></div>',
-          controller: 'BannerController',
-          link: function(scope, iElement, iAttrs) {
-          }
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('banner', function() {
+  var directiveDefinitionObject = {
+    restrict : 'E',
+    priority : 1000,
+    scope : true,
+    //templateUrl: '/app/views/contact-form.html',
+    template : '<div ng-include="getTemplateUrl()"></div>',
+    controller : 'BannerController'
+  };
+  return directiveDefinitionObject;
+});
