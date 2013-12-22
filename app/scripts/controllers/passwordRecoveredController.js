a0333af258e8989a1a375b32e3ef52faa0247740
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:49:07 +0100

    jshint controllers/passwordRecoveredController.js

--- app/scripts/controllers/passwordRecoveredController.js
+++ app/scripts/controllers/passwordRecoveredController.js
@@ -1,23 +1,25 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('PasswordRecoveredController', function ($scope, $rootScope, $routeParams, User) {
-	  if($rootScope.logged) {
-		  $location.path('/account');
-		  return;
-	  }
-		
-	  $scope.data = false;
-	  $scope.passwordRecovered = function(id) {
-		  User.passwordRecovered({'id':id}, function(data) {
-			  $scope.data = data;
-		  }, function(data) {
-			  $scope.data = data.data;
-		  });
-	  }
-	  $scope.passwordRecovered($routeParams.id);
-	  
-	  $scope.getTemplateUrl =  function() {
-		  return '/app/views/loggedout/password-recovered.html';
-	  }
-  });
+angular.module('Neobazaar').controller('PasswordRecoveredController',
+    function($scope, $rootScope, $routeParams, $location, User) {
+      if ($rootScope.logged) {
+        $location.path('/account');
+        return;
+      }
+
+      $scope.data = false;
+      $scope.passwordRecovered = function(id) {
+        User.passwordRecovered({
+          'id' : id
+        }, function(data) {
+          $scope.data = data;
+        }, function(data) {
+          $scope.data = data.data;
+        });
+      };
+      $scope.passwordRecovered($routeParams.id);
+
+      $scope.getTemplateUrl = function() {
+        return '/app/views/loggedout/password-recovered.html';
+      };
+    });
