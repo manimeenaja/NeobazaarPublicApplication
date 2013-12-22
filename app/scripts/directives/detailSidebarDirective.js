e771fc83481c3ffe109ea72d95e6c859f6d2c349
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 03:22:39 +0100

    jshint directives/detailSidebarDirective.js

--- app/scripts/directives/detailSidebarDirective.js
+++ app/scripts/directives/detailSidebarDirective.js
@@ -2,13 +2,13 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('detailSidebar', function () {
-	  var directiveDefinitionObject =  {
-          restrict: 'E',
-          priority: 1000,
-          scope: true,
-          templateUrl: '/app/views/detail-sidebar.html',
-          controller: 'DetailSidebarController'
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('detailSidebar', function() {
+  var directiveDefinitionObject = {
+    restrict : 'E',
+    priority : 1000,
+    scope : true,
+    templateUrl : '/app/views/detail-sidebar.html',
+    controller : 'DetailSidebarController'
+  };
+  return directiveDefinitionObject;
+});
