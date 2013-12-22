17d00f41c5fe53e52910663075e8a7197505eea1
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 01:00:27 +0100

    jshint services/captchaLoaderService.js

--- app/scripts/services/captchaLoaderService.js
+++ app/scripts/services/captchaLoaderService.js
@@ -2,15 +2,16 @@
 
 var services = angular.module('Neobazaar.services');
 
-services.factory('CaptchaLoader', ['Captcha', '$q',
-    function (Captcha, $q) {
-        return function (params) {
-            var delay = $q.defer();
-            Captcha.get({id: params.id}, function (captcha) {
-                delay.resolve(captcha);
-            }, function () {
-                delay.reject('Unable to fetch Captcha');
-            });
-            return delay.promise;
-        };
-    }]);
+services.factory('CaptchaLoader', [ 'Captcha', '$q', function(Captcha, $q) {
+  return function(params) {
+    var delay = $q.defer();
+    Captcha.get({
+      id : params.id
+    }, function(captcha) {
+      delay.resolve(captcha);
+    }, function() {
+      delay.reject('Unable to fetch Captcha');
+    });
+    return delay.promise;
+  };
+} ]);
