678c9959e9e9720c4cfb3eaa426fd703d967c754
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:37:23 +0100

    jshint controllers/registerController.js

--- app/scripts/controllers/registerController.js
+++ app/scripts/controllers/registerController.js
@@ -1,49 +1,53 @@
 'use strict';
 
 angular.module('Neobazaar')
-  .controller('RegisterController', function ($scope, $routeParams, $rootScope, $location, $timeout, CsrfLoader) {
-	  if($rootScope.logged) {
-		  $scope.getTemplateUrl =  function() {
-			  return '/app/views/loggedin/register.html';
-		  }
-		  $timeout(function() {
-			  $location.path('account');
-		  }, $rootScope.redirectionDelay);
-		  return;
-	  }
+    .controller(
+        'RegisterController',
+        function($scope, $routeParams, $rootScope, $location, $timeout,
+            CsrfLoader) {
+          if ($rootScope.logged) {
+            $scope.getTemplateUrl = function() {
+              return '/app/views/loggedin/register.html';
+            };
+            $timeout(function() {
+              $location.path('account');
+            }, $rootScope.redirectionDelay);
+            return;
+          }
 
-	  $scope.reset = function() {
-		  $scope.params = {};
-		  $scope.params.agreeterms = 'NO';
-	  }
+          $scope.reset = function() {
+            $scope.params = {};
+            $scope.params.agreeterms = 'NO';
+          };
 
-	  $scope.resource = new CsrfLoader({});
-	  $scope.resource.then(function(data) {	
-		  if($scope.params) {
-			  $scope.params.csrf = data.id;
-		  }
-	  });
-	  
-	  $scope.prepareRequestParams = function(scope) {
-		  $('#register').block({ message: '<div>Loading</div>' });
-		  
-		  return $scope.params;
-	  }
+          $scope.resource = new CsrfLoader({});
+          $scope.resource.then(function(data) {
+            if ($scope.params) {
+              $scope.params.csrf = data.id;
+            }
+          });
 
-	  $scope.sent = false;
-	  $scope.success = function(data, status, headers, config) {
-		  $rootScope.$emit('registerUser');
-		  $('#register').unblock();
-		  $scope.sent = true;
-	  }
-	  
-	  $scope.error = function(data, status, headers, config) {
-		  $('#register').unblock();
-	  }
-	  
-	  $scope.getTemplateUrl =  function() {
-		  return $scope.sent ? 
-				 '/app/views/loggedout/register-sent.html' : 
-				'/app/views/loggedout/register.html';
-	  }
-  });
+          $scope.prepareRequestParams = function() {
+            angular.element('#register').block({
+              message : '<div>Loading</div>'
+            });
+
+            return $scope.params;
+          };
+
+          $scope.sent = false;
+          $scope.success = function() {
+            $rootScope.$emit('registerUser');
+            angular.element('#register').unblock();
+            $scope.sent = true;
+          };
+
+          $scope.error = function() {
+            angular.element('#register').unblock();
+          };
+
+          $scope.getTemplateUrl = function() {
+            return $scope.sent ? '/app/views/loggedout/register-sent.html'
+                : '/app/views/loggedout/register.html';
+          };
+        });
