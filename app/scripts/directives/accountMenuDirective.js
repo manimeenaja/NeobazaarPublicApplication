8254237c1b64bd3b6be55175347629453051059a
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:20:21 +0100

    jshint directives/accountMenuDirective.js

--- app/scripts/directives/accountMenuDirective.js
+++ app/scripts/directives/accountMenuDirective.js
@@ -2,16 +2,14 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('accountMenu', function () {
-	  var directiveDefinitionObject =  {
-          restrict: 'E',
-          priority: 1000,
-          scope: true,
-          //templateUrl: '/app/views/contact-form.html',
-          template: '<div ng-include="getTemplateUrl()"></div>',
-          controller: 'AccountMenuController',
-          link: function(scope, iElement, iAttrs) {
-          }
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('accountMenu', function() {
+  var directiveDefinitionObject = {
+    restrict : 'E',
+    priority : 1000,
+    scope : true,
+    //templateUrl: '/app/views/contact-form.html',
+    template : '<div ng-include="getTemplateUrl()"></div>',
+    controller : 'AccountMenuController'
+  };
+  return directiveDefinitionObject;
+});
