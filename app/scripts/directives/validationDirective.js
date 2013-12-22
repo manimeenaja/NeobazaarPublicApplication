e58fd90d8e65426c0458d0787847005672c0d950
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 01:18:09 +0100

    jshint directives/validationDirective.js

--- app/scripts/directives/validationDirective.js
+++ app/scripts/directives/validationDirective.js
@@ -2,132 +2,135 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('validation', function ($http) {
-	var directiveDefinitionObject =  {
-		link: function($scope, $element, attrs) {	
-			// Add novalidate to the form element.
-			attrs.$set('novalidate' , 'novalidate');	
-			
-			$scope[attrs.validationmodelinit]();
-			
-			$scope.elementManager = function (element, data, status) {
-				element.find('input, textarea, select').each(function(i, v) {
-					var el = $(v);
-					var name = el.attr('name');
-					
-					if(!name) return;
-					
-					el.removeClass('ng-invalid')
-						.removeClass('ng-valid')
-						.addClass('ng-dirty')
-					  	.addClass('ng-pristine');
-					
-					if(-1 != name.indexOf('[')) {
-						var newname = "[" + name.replace("[", "][");
-						newname = newname.replace(/\[/g, "['");
-						newname = newname.replace(/\]/g, "']");
-						var errors = null;
-						var s = 'errors = data' + newname + ';';
-						try {
-							eval(s);
-						} catch(e) { }
-					} else {
-						var errors = data[name];
-					}
-					
-					// angular element
-					var ngel = $scope[attrs.name][name];
-	
-					if(!ngel) return;
-					
-					if (undefined != errors && 200 != status) {
-						ngel.$dirty = true;
-						ngel.$invalid = true;
-						ngel.$valid = false;
-						ngel.$pristine = false;
-						el.addClass('ng-invalid');
-						ngel.messages = new Array();
-						for(var x in errors) {
-							ngel.messages.push(errors[x]);
-							ngel.$setValidity(x, false);
-						}
-					} else if (200 != status) {
-						ngel.messages = new Array();
-						ngel.$dirty = false;
-						ngel.$invalid = false;
-						ngel.$valid = true;
-						ngel.$pristine = true;
-						el.addClass('ng-valid');
-					} else {
-						ngel.messages = new Array();
-						ngel.$dirty = false;
-						ngel.$invalid = false;
-						ngel.$valid = true;
-						ngel.$pristine = true;
-						el.addClass('ng-valid').removeClass('ng-dirty');
-					}
-				});
-			}
-			
+directives.directive('validation', function($http) {
+  var directiveDefinitionObject = {
+    link : function($scope, $element, attrs) {
+      // Add novalidate to the form element.
+      attrs.$set('novalidate', 'novalidate');
 
-			//$scope[attrs.validationbeforesend]();
-	
-			var element = angular.element($element);
-			element.on('submit', function (evt) {
-				evt.preventDefault();
-				
-				if('validationrequestadata' in attrs && 
-						attrs.validationrequestadata in $scope) {
-					var data = $scope[attrs.validationrequestadata]($scope);
-				} else {
-					var data = $scope[attrs.validationmodel];
-				}
-				
-				var method = 'validationmethod' in attrs ? attrs.validationmethod : 'post';
-				
-				switch(method.toLowerCase()) {
-				case 'put':
-					$http.put(attrs.action, data)
-						.success(function(data, status, headers, config) {
-							$scope.elementManager(element, data, status);
-							if('validationsuccess' in attrs) {
-								$scope[attrs.validationsuccess](data, status, headers, config);
-							}
-						})
-						.error(function(data, status, headers, config) {
-							element.removeClass('ng-pristine');
-							element.addClass('ng-dirty');
-							$scope.elementManager(element, data, status);
+      $scope[attrs.validationmodelinit]();
 
-							if('validationerror' in attrs && 
-									attrs.validationerror in $scope) {
-								$scope[attrs.validationerror](data, status, headers, config);
-							}
-						});
-				break;
-					break;
-				default:
-					$http.post(attrs.action, data)
-						.success(function(data, status, headers, config) {
-							$scope.elementManager(element, data, status);
-							if('validationsuccess' in attrs) {
-								$scope[attrs.validationsuccess](data, status, headers, config);
-							}
-						})
-						.error(function(data, status, headers, config) {
-							element.removeClass('ng-pristine');
-							element.addClass('ng-dirty');
-							$scope.elementManager(element, data, status);
+      $scope.elementManager = function(element, data, status) {
+        element.find('input, textarea, select').each(
+            function(i, v) {
+              var el = v;
+              var name = el.attr('name');
 
-							if('validationerror' in attrs && 
-									attrs.validationerror in $scope) {
-								$scope[attrs.validationerror](data, status, headers, config);
-							}
-						});
-					break;
-				}
-			});
-		}
-	};
-	return directiveDefinitionObject;
+              if (!name) {
+                return;
+              }
+
+              el.removeClass('ng-invalid').removeClass('ng-valid').addClass(
+                  'ng-dirty').addClass('ng-pristine');
+
+              var errors = null;
+              if (-1 !== name.indexOf('[')) {
+                var newname = '[' + name.replace('[', '][');
+                newname = newname.replace(/\[/g, '[\'');
+                newname = newname.replace(/\]/g, '\']');
+                var s = 'errors = data' + newname + ';';
+                try {
+                  /*jslint evil: true */
+                  eval(s);
+                } catch (e) {
+                }
+              } else {
+                errors = data[name];
+              }
+
+              // angular element
+              var ngel = $scope[attrs.name][name];
+
+              if (!ngel) {
+                return;
+              }
+
+              if (undefined !== errors && 200 !== status) {
+                ngel.$dirty = true;
+                ngel.$invalid = true;
+                ngel.$valid = false;
+                ngel.$pristine = false;
+                el.addClass('ng-invalid');
+                ngel.messages = [];
+                for ( var x in errors) {
+                  ngel.messages.push(errors[x]);
+                  ngel.$setValidity(x, false);
+                }
+              } else if (200 !== status) {
+                ngel.messages = [];
+                ngel.$dirty = false;
+                ngel.$invalid = false;
+                ngel.$valid = true;
+                ngel.$pristine = true;
+                el.addClass('ng-valid');
+              } else {
+                ngel.messages = [];
+                ngel.$dirty = false;
+                ngel.$invalid = false;
+                ngel.$valid = true;
+                ngel.$pristine = true;
+                el.addClass('ng-valid').removeClass('ng-dirty');
+              }
+            });
+      };
+
+      // $scope[attrs.validationbeforesend]();
+
+      var element = angular.element($element);
+      element.on('submit', function(evt) {
+        evt.preventDefault();
+
+        var data = null;
+        if ('validationrequestadata' in attrs &&
+            attrs.validationrequestadata in $scope) {
+          data = $scope[attrs.validationrequestadata]($scope);
+        } else {
+          data = $scope[attrs.validationmodel];
+        }
+
+        var method = 'validationmethod' in attrs ? attrs.validationmethod
+            : 'post';
+
+        switch (method.toLowerCase()) {
+        case 'put':
+          $http.put(attrs.action, data).success(
+              function(data, status, headers, config) {
+                $scope.elementManager(element, data, status);
+                if ('validationsuccess' in attrs) {
+                  $scope[attrs.validationsuccess]
+                      (data, status, headers, config);
+                }
+              }).error(function(data, status, headers, config) {
+            element.removeClass('ng-pristine');
+            element.addClass('ng-dirty');
+            $scope.elementManager(element, data, status);
+
+            if ('validationerror' in attrs && attrs.validationerror in $scope) {
+              $scope[attrs.validationerror](data, status, headers, config);
+            }
+          });
+          break;
+        default:
+          $http.post(attrs.action, data).success(
+              function(data, status, headers, config) {
+                $scope.elementManager(element, data, status);
+                if ('validationsuccess' in attrs) {
+                  $scope[attrs.validationsuccess]
+                      (data, status, headers, config);
+                }
+              }).error(function(data, status, headers, config) {
+            element.removeClass('ng-pristine');
+            element.addClass('ng-dirty');
+            $scope.elementManager(element, data, status);
+
+            if ('validationerror' in attrs && attrs.validationerror in $scope) {
+              $scope[attrs.validationerror](data, status, headers, config);
+            }
+          });
+          break;
+        }
+      });
+    }
+  };
+  return directiveDefinitionObject;
 });
