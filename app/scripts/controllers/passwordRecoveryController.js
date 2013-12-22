5c70a376a3f84e96c976213ad6d24f6466a6480a
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:47:56 +0100

    jshint controllers/passwordRecoveryController.js

--- app/scripts/controllers/passwordRecoveryController.js
+++ app/scripts/controllers/passwordRecoveryController.js
@@ -1,45 +1,47 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('PasswordRecoveryController', function ($scope, $rootScope, $routeParams, CsrfLoader) {
-	  if($rootScope.logged) {
-		  $location.path('/account');
-	  }
+angular.module('Neobazaar').controller(
+    'PasswordRecoveryController',
+    function($scope, $rootScope, $routeParams, $location, CsrfLoader) {
+      if ($rootScope.logged) {
+        $location.path('/account');
+      }
 
-	  $scope.response = false; 
-	  $scope.error = function(data, status, headers, config) {
-		  $('#passwordrecovery').unblock();
-		  $scope.response = false; // reset response
-		  $scope.response = data;
-	  }
+      $scope.response = false;
+      $scope.error = function(data) {
+        angular.element('#passwordrecovery').unblock();
+        $scope.response = false; // reset response
+        $scope.response = data;
+      };
 
-	  $scope.sent = false;
-	  $scope.success = function(data, status, headers, config) {
-		  $('#passwordrecovery').unblock();
-		  $scope.sent = true;
-	  }
-	  
-	  $scope.reset = function() {
-		  $scope.params = {};
-		  $scope.params.action = 'passwordrecovery';
-	  }
-	  
-	  $scope.prepareRequestParams = function(scope) {
-		  $('#passwordrecovery').block({ message: '<div>Loading</div>' });
-		  
-		  return $scope.params;
-	  }
+      $scope.sent = false;
+      $scope.success = function() {
+        angular.element('#passwordrecovery').unblock();
+        $scope.sent = true;
+      };
 
-	  $scope.resource = new CsrfLoader({});
-	  $scope.resource.then(function(data) {	
-		  if($scope.params) {
-			  $scope.params.csrf = data.id;
-		  }
-	  });
-	  
-	  $scope.getTemplateUrl =  function() {
-		  return $scope.sent ? 
-				  '/app/views/loggedout/password-recovery-sent.html' : 
-					  '/app/views/loggedout/password-recovery.html';
-	  }
-  });
+      $scope.reset = function() {
+        $scope.params = {};
+        $scope.params.action = 'passwordrecovery';
+      };
+
+      $scope.prepareRequestParams = function() {
+        angular.element('#passwordrecovery').block({
+          message : '<div>Loading</div>'
+        });
+
+        return $scope.params;
+      };
+
+      $scope.resource = new CsrfLoader({});
+      $scope.resource.then(function(data) {
+        if ($scope.params) {
+          $scope.params.csrf = data.id;
+        }
+      });
+
+      $scope.getTemplateUrl = function() {
+        return $scope.sent ? '/app/views/loggedout/password-recovery-sent.html'
+            : '/app/views/loggedout/password-recovery.html';
+      };
+    });
