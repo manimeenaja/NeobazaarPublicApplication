794ad6b4f255a493911b612a494475de5f9986ed
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:49:56 +0100

    jshint controllers/logoutController.js

--- app/scripts/controllers/logoutController.js
+++ app/scripts/controllers/logoutController.js
@@ -1,31 +1,31 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('LogoutController', function ($scope, $rootScope, $location, $timeout, LoggedOutLoader) {
-	  if(!$rootScope.logged) {
-		  $scope.getTemplateUrl =  function() {
-			  return '/app/views/loggedout/logout.html';
-		  }
-		  $timeout(function() {
-			  $location.path('/');
-		  }, $rootScope.redirectionDelay);
-		  return;
-	  }
-	  
-	  $scope.check = function() {
-		  $scope.resource = new LoggedOutLoader({});
-		  $scope.resource.then(function(data) {	
-			  if(data.logout) {
-				  $rootScope.$emit('loggedOut');
-				  $timeout(function() {
-					  $location.path('/');
-				  }, $rootScope.redirectionDelay);
-			  }
-		  }); 
-	  }
-	  $scope.check();
+angular.module('Neobazaar').controller('LogoutController',
+    function($scope, $rootScope, $location, $timeout, LoggedOutLoader) {
+      if (!$rootScope.logged) {
+        $scope.getTemplateUrl = function() {
+          return '/app/views/loggedout/logout.html';
+        };
+        $timeout(function() {
+          $location.path('/');
+        }, $rootScope.redirectionDelay);
+        return;
+      }
 
-	  $scope.getTemplateUrl =  function() {
-		  return '/app/views/loggedin/logout.html';
-	  }
-  });
+      $scope.check = function() {
+        $scope.resource = new LoggedOutLoader({});
+        $scope.resource.then(function(data) {
+          if (data.logout) {
+            $rootScope.$emit('loggedOut');
+            $timeout(function() {
+              $location.path('/');
+            }, $rootScope.redirectionDelay);
+          }
+        });
+      };
+      $scope.check();
+
+      $scope.getTemplateUrl = function() {
+        return '/app/views/loggedin/logout.html';
+      };
+    });
