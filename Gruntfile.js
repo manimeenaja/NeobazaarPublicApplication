3423c4aca2c2f6e32960c19d9ec129daf843f0eb
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 21:34:29 +0100

    jshintb Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -274,11 +274,11 @@ module.exports = function (grunt) {
           rename: function (dest, src) {
               var path = require('path');
               if (src === 'distpackage.json') {
-                  return path.join(dest, 'package.json');
+                return path.join(dest, 'package.json');
               }
               return path.join(dest, src);
-          }
-      }]
+            }
+        }]
       },
       styles: {
         expand: true,
