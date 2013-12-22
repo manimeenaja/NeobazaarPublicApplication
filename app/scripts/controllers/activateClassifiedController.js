68c71739c365663d5366b67a44f164463d3410ad
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 21:23:29 +0100

    jshint controllers/activateClassifiedController.js

--- app/scripts/controllers/activateClassifiedController.js
+++ app/scripts/controllers/activateClassifiedController.js
@@ -1,28 +1,25 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('ActivateClassifiedController', function($scope, $routeParams, $rootScope, $location, $http, Classified) {
-	  if($rootScope.logged) {
-		  $location.path('account');
-		  return;
-	  }
-//		Il tuo annuncio è stato attivato, ma <strong>non è ancora stato pubblicato</strong>.<br />
-//		Abbiamo inviato un'email all'indirizzo che ci hai segnalato con il link da cliccare per l'attivazione.<br />
-//		Se non hai ancora ricevuto l'email, controlla anche nella cartella dello spam.<br />
+angular.module('Neobazaar').controller('ActivateClassifiedController',
+    function($scope, $routeParams, $rootScope, $location, $http, Classified) {
+      if ($rootScope.logged) {
+        $location.path('account');
+        return;
+      }
 
-	  $scope.data = null;
-	  $scope.activateClassified = function() {
-		  Classified.activation({'id':$routeParams.id}, function(data) {
-			  $scope.data = data;
-		  }, function(data) {
-			  $scope.data = data.data;
-		  });
-	  }
-	  $scope.activateClassified();
-	  
-	  
-	$scope.getTemplateUrl = function() {
-		return '/app/views/loggedout/activate-classified.html';
-	}; 
-});
-	 
+      $scope.data = null;
+      $scope.activateClassified = function() {
+        Classified.activation({
+          'id' : $routeParams.id
+        }, function(data) {
+          $scope.data = data;
+        }, function(data) {
+          $scope.data = data.data;
+        });
+      };
+      $scope.activateClassified();
+
+      $scope.getTemplateUrl = function() {
+        return '/app/views/loggedout/activate-classified.html';
+      };
+    });
