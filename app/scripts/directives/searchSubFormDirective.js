7f0f0140e5aefe9053a4d8fcfe85bb781c2a9da1
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 02:57:40 +0100

    jshint directives/searchPanelDirective.js

--- app/scripts/directives/searchSubFormDirective.js
+++ app/scripts/directives/searchSubFormDirective.js
@@ -2,11 +2,11 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('neoSearchSubForm', function ($compile) {
-    return {
-        restrict: "E",
-        rep1ace: true,
-        controller: 'SearchSubFormController',
-        template: '<div ng-include="getTemplateUrl()"></div>'
-    };
+directives.directive('neoSearchSubForm', function() {
+  return {
+    restrict : 'E',
+    rep1ace : true,
+    controller : 'SearchSubFormController',
+    template : '<div ng-include="getTemplateUrl()"></div>'
+  };
 });
