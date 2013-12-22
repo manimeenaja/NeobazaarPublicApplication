5a6c9819d1c6b683a31b16990e22e07a938b0370
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 20:00:50 +0100

    jshint controllers/adminClassifiedController.js

--- app/scripts/controllers/adminClassifiedController.js
+++ app/scripts/controllers/adminClassifiedController.js
@@ -2,57 +2,55 @@
 
 angular.module('Neobazaar')
   .controller('AdminClassifiedController', function ($scope, $routeParams, $route, $rootScope, $location, $timeout, Classified, ClassifiedsLoader) {
-//	  if(!$rootScope.logged) {
-//		  $scope.getTemplateUrl =  function() {
-//			  return '/app/views/loggedout/account.html';
-//		  }
-//		  $timeout(function() {
-//			  $location.path('/login');
-//		  }, $rootScope.redirectionDelay);
-//		  return;
-//	  }
+	  if(!$rootScope.logged) {
+		  $scope.getTemplateUrl =  function() {
+			  return '/app/views/loggedout/account.html';
+		  };
+		  $timeout(function() {
+			  $location.path('/login');
+		  }, $rootScope.redirectionDelay);
+		  return;
+	  }
 	  
 	  $scope.isAdmin = true;
 	  $scope.disableEnable = function(item) {
-		  Classified.disableEnable({'id':item.hashId}, function(data) {
+		  Classified.disableEnable({'id':item.hashId}, function() {
 			  $route.reload();
-		  }, function(data) {
-			  
+		  }, function() {
 		  });
-	  }
+	  };
 	  
 	  $scope.delete = function(item) {
+	    /*jshint devel:true*/
 		  if(confirm('Questo eliminerà l\'annuncio e le immagini associate, l\'azione è irreversibile, procedi?')) {
-			  Classified.remove({'id':item.hashId}, function(data) {
+			  Classified.remove({'id':item.hashId}, function() {
 				  $route.reload();
-			  }, function(data) {
-				  
+			  }, function() {
 			  });
 		  }
-	  }
+	  };
 	  
 	  $scope.deleteSelected = function() {
 		  if(confirm('Questo eliminerà tutti gli annunci selezionati, procedi?')) {
 			  var ids = [];
-			  $('.full-check:checked').each(function(i, v) {
-				  ids.push($(v).attr('id'));
+			  angular.element('.full-check:checked').each(function(i, v) {
+				  ids.push(angular.element(v).attr('id'));
 			  });
 			  
-			  if(ids.length) { 
-				  Classified.remove({'id': 'bulk'}, {'ids':ids}, function(data) {
+			  if(ids.length) {
+				  Classified.remove({'id': 'bulk'}, {'ids':ids}, function() {
 					  $route.reload();
-				  }, function(data) {
-				  
+				  }, function() {
 				  });
 			  }
 
 			  return false;
 		  }
-	  }
+	  };
 	  
 	  $scope.fullCheck = function(e) {
-		  $('.full-check').prop('checked', $(e.target).prop('checked'));
-	  }
+	    angular.element('.full-check').prop('checked', angular.element(e.target).prop('checked'));
+	  };
 		
 	  $routeParams.full = 1;
 	  $scope.resource = new ClassifiedsLoader($routeParams);
@@ -60,5 +58,5 @@ angular.module('Neobazaar')
 	  
 	  $scope.getTemplateUrl =  function() {
 		  return '/app/views/loggedin/classifieds.html';
-	  }
+	  };
   });
