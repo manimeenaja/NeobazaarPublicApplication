2aa72c8f1857c86a13ef4ef8bb58ddee996d4c2f
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:17:40 +0100

    jshint directives/breadcrumbsDirective.js

--- app/scripts/directives/breadcrumbsDirective.js
+++ app/scripts/directives/breadcrumbsDirective.js
@@ -2,13 +2,13 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('neoBreadcrumbs', function () {
-	  var directiveDefinitionObject =  {
-          restrict: 'E',
-          priority: 1000,
-          scope: true,
-          templateUrl: '/app/views/breadcrumbs.html',
-          controller: 'BreadcrumbsController'
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('neoBreadcrumbs', function() {
+  var directiveDefinitionObject = {
+    restrict : 'E',
+    priority : 1000,
+    scope : true,
+    templateUrl : '/app/views/breadcrumbs.html',
+    controller : 'BreadcrumbsController'
+  };
+  return directiveDefinitionObject;
+});
