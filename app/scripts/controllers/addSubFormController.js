1e99f7b3be91d7034a2ed70da9c8be344e0f8127
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 20:02:18 +0100

    jshint controllers/addSubFormController.js

--- app/scripts/controllers/addSubFormController.js
+++ app/scripts/controllers/addSubFormController.js
@@ -1,44 +1,42 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('AddSubFormController', function($scope, $rootScope, $http) {
+angular.module('Neobazaar').controller(
+    'AddSubFormController',
+    function($scope, $rootScope, $http) {
 
-	var map = $rootScope.categoryMap;
-	  
-	$scope.$watch('classified.category.value', function() {
-		if(!$scope.classified.category) {
-			return;
-		}
-		
-		var c = $scope.classified.category.value;
-		c = undefined == c ? 0 : c;
-		c = map[c];
-		c = undefined == c ? 'empty' : c;
-		$scope.getTemplateUrl = function() {
-			return '/app/views/addsubform/' + c + '.html';
-		};
-	});
-	  
-	$scope.$watch('classified.location.value', function() {
-		if(!$scope.classified.location) {
-			return;
-		}
-		
-		var c = $scope.classified.location.value;
-		c = undefined == c ? 0 : c;
-		
-		$http.get('/location/' + c + '?action=children')
-		.success(function(data, status, headers, config) {
-			$rootScope.comune = data[0];
-			$rootScope.$emit('subformLoaded');
-		})
-		.error(function(data, status, headers, config) {
+      var map = $rootScope.categoryMap;
 
-		});
-	});
+      $scope.$watch('classified.category.value', function() {
+        if (!$scope.classified.category) {
+          return;
+        }
 
-	$scope.getTemplateUrl = function() {
-		return '/app/views/empty.html';
-	}; 
-});
-	 
+        var c = $scope.classified.category.value;
+        c = undefined === c ? 0 : c;
+        c = map[c];
+        c = undefined === c ? 'empty' : c;
+        $scope.getTemplateUrl = function() {
+          return '/app/views/addsubform/' + c + '.html';
+        };
+      });
+
+      $scope.$watch('classified.location.value', function() {
+        if (!$scope.classified.location) {
+          return;
+        }
+
+        var c = $scope.classified.location.value;
+        c = undefined === c ? 0 : c;
+
+        $http.get('/location/' + c + '?action=children').success(
+            function(data) {
+              $rootScope.comune = data[0];
+              $rootScope.$emit('subformLoaded');
+            }).error(function() {
+        });
+      });
+
+      $scope.getTemplateUrl = function() {
+        return '/app/views/empty.html';
+      };
+    });
