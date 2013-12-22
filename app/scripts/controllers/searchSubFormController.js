f6b9c43108637192e270d9ab33580fc798389a0f
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:27:21 +0100

    jshint controllers/searchSubFormController.js

--- app/scripts/controllers/searchSubFormController.js
+++ app/scripts/controllers/searchSubFormController.js
@@ -1,22 +1,21 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('SearchSubFormController', function($scope, $rootScope) {
-	  var map = $rootScope.categoryMap;
-	  
-	  $scope.$watch('params.category.value', function() {
-			var c = $scope.params.category.value;
-			c = undefined == c ? 0 : c;
-			c = map[c];
-			c = undefined == c ? 'empty' : c;
-		  $scope.getTemplateUrl = function() {
-			  return '/app/views/searchsubform/' + c + '.html';
-		  };
-	  });
+angular.module('Neobazaar').controller('SearchSubFormController',
+    function($scope, $rootScope) {
+      var map = $rootScope.categoryMap;
 
-	  $scope.getTemplateUrl = function() {
-		  return '/app/views/searchsubform/empty.html';
-	  };
-	  
-  });
-	 
+      $scope.$watch('params.category.value', function() {
+        var c = $scope.params.category.value;
+        c = undefined === c ? 0 : c;
+        c = map[c];
+        c = undefined === c ? 'empty' : c;
+        $scope.getTemplateUrl = function() {
+          return '/app/views/searchsubform/' + c + '.html';
+        };
+      });
+
+      $scope.getTemplateUrl = function() {
+        return '/app/views/searchsubform/empty.html';
+      };
+
+    });
