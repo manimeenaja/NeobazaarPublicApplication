c2ba91492ce6b9435fa8171ba81e367ee24a0d75
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 03:14:42 +0100

    jshint directives/headerDirective.js

--- app/scripts/directives/headerDirective.js
+++ app/scripts/directives/headerDirective.js
@@ -2,14 +2,14 @@
 
 var directives = angular.module('Neobazaar.directives');
 
-directives.directive('neoHeader', function () {
-	  var directiveDefinitionObject =  {
-          restrict: 'E',
-          priority: 1000,
-          scope: true,
-          //templateUrl: '/app/views/header.html',
-          template: '<div ng-include="getHeaderTemplateUrl()"></div>',
-          controller: 'HeaderController'
-      };
-	  return directiveDefinitionObject;
-  });
+directives.directive('neoHeader', function() {
+  var directiveDefinitionObject = {
+    restrict : 'E',
+    priority : 1000,
+    scope : true,
+    //templateUrl: '/app/views/header.html',
+    template : '<div ng-include="getHeaderTemplateUrl()"></div>',
+    controller : 'HeaderController'
+  };
+  return directiveDefinitionObject;
+});
