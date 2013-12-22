64a633622d9cc37e356691cd72da8f863dcba0be
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2013-12-22 18:52:09 +0100

    jshint controllers/loggedInController.js

--- app/scripts/controllers/loggedInController.js
+++ app/scripts/controllers/loggedInController.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('Neobazaar')
-  .controller('LoggedInController', function ($scope, $rootScope) {
-	  $scope.getTemplateUrl = function() {
-		  return $rootScope.logged ? 
-				'/app/views/loggedin/top-right-buttons.html' : 
-				'/app/views/loggedout/top-right-buttons.html';
-	  }
-  });
+angular.module('Neobazaar').controller(
+    'LoggedInController',
+    function($scope, $rootScope) {
+      $scope.getTemplateUrl = function() {
+        return $rootScope.logged ? '/app/views/loggedin/top-right-buttons.html'
+            : '/app/views/loggedout/top-right-buttons.html';
+      };
+    });
