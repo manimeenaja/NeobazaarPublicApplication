90926bbdab58db2dba3191a1059cf6cfe6afe48f
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2014-02-27 00:31:42 +0100

    Fixing detail wip

--- app/scripts/controllers/detailController.js
+++ app/scripts/controllers/detailController.js
@@ -3,10 +3,17 @@
 angular.module('Neobazaar').controller(
     'DetailController',
     function($scope, $rootScope, $http, $location, $window, ClassifiedLoader) {
-      $scope.resource = new ClassifiedLoader();
-      $scope.path = $location.path();
+      
 
-      $scope.resource.then(function(response) {
+      $scope.path = $location.path();
+      
+      var loader = new ClassifiedsLoader();
+      $scope.resource = loader.then(function(data) {
+        $scope.resource = data;
+        
+        console.log($scope.resource);
+        
+
         $window.document.title = response.data.title + ' - ' +
             ' Neobazaar annunci gratuiti';
         $rootScope.fb = {
@@ -14,9 +21,35 @@ angular.module('Neobazaar').controller(
           sitename : 'Neobazaar',
           description : 'Neobazaar free classifieds'
         };
+        
       });
+      
+      
+      
+      
+      
+      /*
+      $scope.resource = new ClassifiedLoader();
+      $scope.path = $location.path();
+
+      $scope.resource.then(function(response) {
+        
+        
+        $window.document.title = response.data.title + ' - ' +
+            ' Neobazaar annunci gratuiti';
+        $rootScope.fb = {
+          title : $window.document.title,
+          sitename : 'Neobazaar',
+          description : 'Neobazaar free classifieds'
+        };
+        
+        
+      });*/
 
       $scope.back = function() {
         $window.history.back();
       };
+      
+      
+      
     });
