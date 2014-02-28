547e9d1457200e50b12af9f89e6cd7c4fc06e715
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2014-02-28 21:33:09 +0100

    open graph tag fix

--- app/scripts/controllers/detailController.js
+++ app/scripts/controllers/detailController.js
@@ -3,54 +3,22 @@
 angular.module('Neobazaar').controller(
     'DetailController',
     function($scope, $rootScope, $http, $location, $window, ClassifiedLoader) {
-      
-
       $scope.path = $location.path();
       
       var loader = new ClassifiedLoader();
       $scope.resource = loader.then(function(data) {
         $scope.resource = data;
-        
-        console.log(data.data);
 
-        $window.document.title = data.data.title + ' - ' +
-            ' Neobazaar annunci gratuiti';
+        $window.document.title = data.data.title + ' - Neobazaar annunci gratuiti';
         $rootScope.fb.title = $window.document.title;
         $rootScope.fb.type = 'object';
         $rootScope.fb.image = data.data.image.src;
         $rootScope.fb.url = $location.absUrl();
         $rootScope.fb.description = data.data.content;
         $rootScope.fb.sitename = 'Neobazaar annunci gratuiti';
-       
-        
       });
-      
-      
-      
-      
-      
-      /*
-      $scope.resource = new ClassifiedLoader();
-      $scope.path = $location.path();
-
-      $scope.resource.then(function(response) {
-        
-        
-        $window.document.title = response.data.title + ' - ' +
-            ' Neobazaar annunci gratuiti';
-        $rootScope.fb = {
-          title : $window.document.title,
-          sitename : 'Neobazaar',
-          description : 'Neobazaar free classifieds'
-        };
-        
-        
-      });*/
 
       $scope.back = function() {
         $window.history.back();
       };
-      
-      
-      
-    });
+});
