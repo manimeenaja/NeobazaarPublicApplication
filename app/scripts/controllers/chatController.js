55cfa7e043d10bbca5d8f3f26fc21f7fb5816aec
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2014-04-15 01:11:52 +0200

    Added chat controller

--- app/scripts/controllers/chatController.js
+++ app/scripts/controllers/chatController.js
@@ -3,6 +3,16 @@
 angular.module('Neobazaar')
   .controller('ChatController', function ($scope) {
 
+    var conn = new WebSocket('ws://www.neobazaar.com:8080');
+    conn.onopen = function(e) {
+        console.log("Connection established!");
+    };
+
+    conn.onmessage = function(e) {
+        console.log(e.data);
+    };
+    
+    
 	  $scope.getTemplateUrl = function() {
 		  return '/views/chat.html';
 	  };
