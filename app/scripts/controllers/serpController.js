5b3283147517143d6edb9bbbc10a1906b4f15160
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2014-04-21 03:08:02 +0200

    fix

--- app/scripts/controllers/serpController.js
+++ app/scripts/controllers/serpController.js
@@ -22,19 +22,23 @@ angular.module('Neobazaar')
       }];
     });
     
-    var conn = new ab.Session(
-        'ws://www.neobazaar.com:8080', 
-        function() {            
-          conn.subscribe('allCategories', function(topic, data) {
-            console.log('New article published to category "' + topic + '" : ' + data.title);
-          });
-        }, 
-        function() {            
-          console.warn('WebSocket connection closed');
-        }, 
-        { // Additional parameters, we're ignoring the WAMP sub-protocol for older browsers
-          'skipSubprotocolCheck': true
-        }
-    );
+    try {
+      var conn = new ab.Session(
+          'ws://localhost:8080', 
+          function() {            
+            conn.subscribe('allCategories', function(topic, data) {
+              console.log('New article published to category "' + topic + '" : ' + data.title);
+            });
+          }, 
+          function() {            
+            console.warn('WebSocket connection closed');
+          }, 
+          { // Additional parameters, we're ignoring the WAMP sub-protocol for older browsers
+            'skipSubprotocolCheck': true
+          }
+      );
+    } catch(e) {
+        console.log(e);
+    }
     
   });
